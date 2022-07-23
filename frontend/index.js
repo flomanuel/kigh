6703723d3674da747f38786b8da824fdd3fd0b31
flomanuel <flomanuel.github@gmail.com>
2022-07-23 21:50:21 +0200

    Add fonts, material ui, react, npm

---
+++ frontend/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+import "./styles/main.scss";
+import "./styles/Roboto.scss";
+import "./styles/Google-material-icons.scss";
+
+
+ReactDOM.render(
+    <>
+        <h1>Hello World</h1>
+        <Button variant="contained">Hello World</Button>
+    </>,
+    document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: path.join(__dirname, "frontend", "index.js"),
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                use: ['@svgr/webpack'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    },
+                }
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "frontend", "index.html"),
+        }),
+    ],
+
+}
