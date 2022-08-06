da7b914c4487a60419b4097326d096e33890b8f1
flomanuel <flomanuel.github@gmail.com>
2022-08-06 13:55:35 +0200

    delete unused stylesheet

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import "./styles/main.scss";
 
 import Main from "./pages/Main";
 import NewEntry from "./pages/NewEntry";
